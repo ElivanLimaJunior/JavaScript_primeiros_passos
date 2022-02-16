5152fa3c17d64ac84cfadcbe23b6b6a4da67fb26
Elivan-Jr <elivanlimajunior@gmail.com>
2022-02-16 19:52:31 -0300

    Primeiras Classes
    Aprendi um pouco sobre as classes e criei a minha primeira classe pra o banco.

--- Orientação a Objetos/02-PrimeiraClasse.js
+++ Orientação a Objetos/02-PrimeiraClasse.js
@@ -1,16 +1,28 @@
-const cliente1nome = "Ricardo";
-const cleinte1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
 
-const cliente2nome = "Alice";
-const cleinte2CPF = 88822233387;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Alice";
+cliente1.cpf = 88822233387;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 123456789;
+
+cliente2.nome = "Ricardo";
+cliente2.cpf = 11122233309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
 
 const cliente3nome = "Elivan";
-const cleinte3CPF = 22233311109;
+const cliente3CPF = 22233311109;
 const cliente3Agencia = 1001;
 const cliente3Saldo = 0;
 
-console.log(cliente1nome)
+console.log(cliente1, cliente2)
