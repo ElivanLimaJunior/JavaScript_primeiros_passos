e77420e7e7aabd322c42700ed3dfae815af39af3
Elivan-Jr <elivanlimajunior@gmail.com>
2022-02-16 19:33:12 -0300

    Entendendo a condição

---
+++ Orientação a Objetos/01-EntendendoProblemaCliente.js
@@ -0,0 +1,18 @@
+const cliente1nome = "Ricardo";
+const cleinte1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2nome = "Alice";
+const cleinte2CPF = 88822233387;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3nome = "Elivan";
+const cleinte3CPF = 22233311109;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1nome)
+
+/* Trabalharemos em um novo projeto, encomendado pelo banco Bytebank, no qual criaremos um sistema que permite o cadastro de clientes e contas-correntes, além das operações bancárias mais comuns. */
---
+++ Orientação a Objetos/02-PrimeiraClasse.js
@@ -0,0 +1,16 @@
+const cliente1nome = "Ricardo";
+const cleinte1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2nome = "Alice";
+const cleinte2CPF = 88822233387;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3nome = "Elivan";
+const cleinte3CPF = 22233311109;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1nome)
