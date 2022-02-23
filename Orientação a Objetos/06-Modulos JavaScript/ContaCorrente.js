690f97dbd9d7bb9ca307fff1588c8d21a382415a
Elivan-Jr <elivanlimajunior@gmail.com>
2022-02-23 17:10:32 -0300

    pequenas mudanças

--- Orientação a Objetos/06-Modulos JavaScript/ContaCorrente.js
+++ Orientação a Objetos/06-Modulos JavaScript/ContaCorrente.js
@@ -1,15 +1,26 @@
 /*Quando temos um arquivo criado com a primeira letra maiscula, representa que esse arquivo JS é uma classe*/
-import {Cliente} from "./Cliente.js";
+import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     agencia;
     _cliente;
-    _Saldo;
-    
+
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
-        this._cliente = novoValor;
+            this._cliente = novoValor;
         }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+    
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor){
@@ -18,12 +29,19 @@ export class ContaCorrente{
             return valor;
         }
     }
+
     depositar(valor){
-        if(valor <=0) return;
-        this._saldo += valor;
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
     }
-    transferir(valor, conta){
+
+    tranferir(valor, conta){
+        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
